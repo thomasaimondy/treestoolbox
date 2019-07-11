c7b1058174ed2112ab4b0fb885ce0592407a8f9f
thomasaimondy <779573161@qq.com>
2019-07-11 10:52:09 +0800

    update

--- casia/construct_neuron/thomasTest.m
+++ casia/construct_neuron/thomasTest.m
@@ -3,12 +3,25 @@ start_trees;
 %tree = load_tree('block_27048.CNG.swc');
 %tree = load_tree('HB140930-1-WNC10S10reg.CNG.swc');
 tree = load_tree('NCB8R.CNG.swc');
-
 %tree = load_tree('sample.mtr');
 tree = repair_tree(tree);
 %sse = sse_tree(tree);
 
-botree = BO_tree(tree);
+bo = BO_tree(tree);  % branch order
+[bi, bins, bh] = bin_tree(tree,[],4); % bin sets
+b = B_tree(tree)';    % branch points
+asym = asym_tree(tree, T_tree(tree))'; % balanced or not
+t = T_tree(tree); % get the terminal points
+pl = PL_tree(tree); % the topological path length
+lo = LO_tree(tree); % level order values
+clf;
+%plot_tree(tree, diag(sse));
+%plot_tree(tree,bo);
+%plot_tree(tree,bi);
+%plot_tree(tree,b);
+plot_tree(tree,asym);
+colorbar;
+
 %clf;imagesc(sse);
 %axis equal;
 
@@ -16,7 +29,4 @@ botree = BO_tree(tree);
 %x3d_tree(tree,[],[1,0,0],[],[],'-o ->');
 %pov_tree(temptree,[],eucl_tree(temptree),'-b -s1 -w ->')
 
-clf;
-%plot_tree(tree, diag(sse));
-plot_tree(tree,botree);
-colorbar;
+
