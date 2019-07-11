0b9235359760bd479ff6369407c9c9c7e55c55a4
thomasaimondy <779573161@qq.com>
2019-07-11 10:54:32 +0800

    update

--- casia/construct_neuron/thomasTest.m
+++ casia/construct_neuron/thomasTest.m
@@ -1,12 +1,17 @@
 
 start_trees;
+
+%% loading the data
 %tree = load_tree('block_27048.CNG.swc');
 %tree = load_tree('HB140930-1-WNC10S10reg.CNG.swc');
 tree = load_tree('NCB8R.CNG.swc');
 %tree = load_tree('sample.mtr');
-tree = repair_tree(tree);
-%sse = sse_tree(tree);
 
+%% refine the data
+tree = repair_tree(tree);
+
+%% process the data
+%sse = sse_tree(tree);
 bo = BO_tree(tree);  % branch order
 [bi, bins, bh] = bin_tree(tree,[],4); % bin sets
 b = B_tree(tree)';    % branch points
@@ -14,19 +19,18 @@ asym = asym_tree(tree, T_tree(tree))'; % balanced or not
 t = T_tree(tree); % get the terminal points
 pl = PL_tree(tree); % the topological path length
 lo = LO_tree(tree); % level order values
+% adding more rules here !!!!!!!!
+
+
+
+%% plot the data
 clf;
 %plot_tree(tree, diag(sse));
 %plot_tree(tree,bo);
 %plot_tree(tree,bi);
 %plot_tree(tree,b);
-plot_tree(tree,asym);
+%plot_tree(tree,asym);
+plot_tree(tree,lo);
 colorbar;
-
-%clf;imagesc(sse);
-%axis equal;
-
-%plot_tree(1,BO_tree(temptree));
-%x3d_tree(tree,[],[1,0,0],[],[],'-o ->');
-%pov_tree(temptree,[],eucl_tree(temptree),'-b -s1 -w ->')
 
 
