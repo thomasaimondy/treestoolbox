3d8019d8fc412de2c0c1245c604430f4a4785acc
unknown <605346577@qq.com>
2019-07-15 10:56:21 +0800

    update

--- casia/construct_neuron/construct.m
+++ casia/construct_neuron/construct.m
@@ -1,20 +1,26 @@
-file1 = strcat('D:\treestoolbox\typical\0\154.swc');
-tree1 = load_tree(file1);
-tree1 = repair_tree(tree1);
-num = 500;
+clear;
+file1 = 'D:\treestoolbox\typical\0\100.swc';
+tree1 = repair_tree(load_tree(file1));
+B = B_tree (tree1);
+Blist = find(B);
+num = 1000;
 for i = 1 : num
-    length = size(tree1.X,1);
-    k = 1 + round((i-1)/num*(length/2));%随机去掉i*个点
-    E = round(length*rand(k,1));%生成的是一个i个元素的行向量，元素为1-100之间的数。去掉i个点，去掉点的编号范围为2~9212
-    
-    dtree = delete_tree (tree1, E);
-    rtree = repair_tree(dtree);
-    file2 =  strcat('D:\treestoolbox\new_swc2\',num2str(0),'\',num2str(i),'.swc');
-    swc_tree(rtree,file2);
-    f = figure(i);
-    plot_tree(rtree);
-    set(f,'visible','off')
-    file3 = strcat('D:\treestoolbox\new_tree2\',num2str(0),'\',num2str(i));
-    %imwrite(f,file3);
-    saveas(f,file3,'tif')
+    try
+        index = randi([1,size(Blist,1)],1,1); % 生成随机的branch节点
+        [sub, subtree] = sub_tree (tree1, Blist(index));
+        resttree = delete_tree (tree1, find(sub));
+        index = randi([1,size(Blist,1)-1],1,1); % 剪第2个枝
+        [sub, subtree] = sub_tree (tree1, Blist(index));
+        resttree = delete_tree (resttree, find(sub));
+        rtree = repair_tree(resample_tree (resttree, 1));
+        jtree = jitter_tree(rtree,.1);
+        file2 =  strcat('D:\treestoolbox\new_swc2\',num2str(0),'\',num2str(i),'.swc');
+        swc_tree(jtree,file2);
+        f = figure(i);
+        plot_tree(jtree);
+        set(f,'visible','off')
+        file3 = strcat('D:\treestoolbox\new_tree2\',num2str(0),'\',num2str(i));
+        %imwrite(f,file3);
+        saveas(f,file3,'tif')
+    end
 end
